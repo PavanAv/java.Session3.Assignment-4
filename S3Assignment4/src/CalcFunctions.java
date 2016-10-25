ff71c8e08792d3c10fa2d6a24d9fc0b688d0b3f3
PavanAv <pavanaluri2011@gmail.com>
2016-10-25 16:07:17 +0530

    Session3Assignment4
    Session3Assignment4

---
+++ S3Assignment4/src/CalcFunctions.java
@@ -0,0 +1,18 @@
+public class CalcFunctions {
+public int sum (int x, int y){	
+int res = x + y;		
+return res;
+}	
+public int subtract (int x, int y){		
+int res = x - y;		
+return res;
+}	
+public int multiply (int x, int y) {		
+int res = x * y;		
+return res;
+}
+public double divide (int x, int y){		
+double res = x/y;		
+return res;
+}
+}
